d902c26b775552592207af790695d955d74d883a
Colin <7887972+cln-io@users.noreply.github.com>
2022-03-12 12:56:00 +0100

    Let PHP json encode, and not do it manually ❤ + add a sync gap value

--- dashboard/public/HeliumStatus.php
+++ dashboard/public/HeliumStatus.php
@@ -3,8 +3,15 @@
 	$live_height = trim(file_get_contents('/var/dashboard/statuses/current_blockheight'));
 	$online_status = trim(file_get_contents("/var/dashboard/statuses/online_status"));
 	$miner_height = trim(file_get_contents("/var/dashboard/statuses/infoheight"));
+	$sync_gap = $live_height - $miner_height;
 
 	header('Content-Type:application/json; charset=utf-8');
-
-	echo '{"live_height":"'.$live_height.'","online_status":"'.$online_status.'","miner_height":"'.$miner_height.'"}';
+	$json_to_output = 
+	[
+		"live_height" => $live_height,
+		"online_status" => $online_status,
+		"miner_height" => $miner_height,
+		"sync_gap" => $sync_gap,
+	];
+	echo json_encode($json_to_output);
 ?>
